52296d239e15e4df5f017002fb1b80c65ac3e45c
nghia nguyen <nghiaturtle2002@gmail.com>
2022-11-13 13:32:05 +0700

    'changes'

--- mavenproject1/src/main/java/com/nvn/myapp/Demo.java
+++ mavenproject1/src/main/java/com/nvn/myapp/Demo.java
@@ -11,5 +11,6 @@ package com.nvn.myapp;
 public class Demo {
     public static void main(String[] args){
         System.out.println("Hello github");
+        System.out.println("I'm MeoUMapDit");
     }
 }
